dcb0f3ca89a81ea22af6160a48aafb2d343b07ef
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-07-29 20:20:07 -0300

    Get Users

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ remove_action('rest_api_init', 'create_initial_rest_routes', 99);
 $dirbase = get_template_directory();
 
 require_once $dirbase . '/endpoints/user_post.php';
+require_once $dirbase . '/endpoints/user_get.php';
 
 // Modifica o prefixo da API de wp-json para json apenas
 // Necessário salvar os permalinks para dar um refresh nos URL's
@@ -12,4 +13,10 @@ function change_api($slug) {
   return 'json';
 }
 add_filter('rest_url_prefix', 'change_api');
+
+function expire_token() {
+  return time() + (60 * 60 * 24);
+}
+
+add_action( 'jwt_auth_expire', 'expire_token' );
 ?>
