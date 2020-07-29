22ee919bf0fa47356ab64ea4e10c150487a3c41a
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-07-29 16:14:25 -0300

    Route post user

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+// Remove as rotas definidas pelo WordPress
+remove_action('rest_api_init', 'create_initial_rest_routes', 99);
+
+$dirbase = get_template_directory();
+
+require_once $dirbase . '/endpoints/user_post.php';
+
+// Modifica o prefixo da API de wp-json para json apenas
+// Necessário salvar os permalinks para dar um refresh nos URL's
+function change_api($slug) {
+  return 'json';
+}
+add_filter('rest_url_prefix', 'change_api');
+?>
